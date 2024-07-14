bd9fbd7180040d7623bbd94e6f1da13b41d23256
Nydia Massiel Sánchez <nydsanchez@gmail.com>
2024-07-13 21:16:21 -0600

    feat(activities): create CRUD controller and configure routes for activities

---
+++ server/src/controllers/activities.js
@@ -0,0 +1,127 @@
+const { Activity, Country } = require("../db");
+
+const dataComplete = async (req, res, next) => {
+  const { name, duration, difficulty, season, countries } = req.body;
+  try {
+    if (
+      !name ||
+      !duration ||
+      !difficulty ||
+      !season ||
+      !countries ||
+      !Array.isArray(countries)
+    ) {
+      return res.status(400).json({ message: "Datos faltantes o invalidos" });
+    }
+    next();
+  } catch (error) {
+    console.error("Error in getActivities:", error);
+    res.status(500).send(error.message);
+  }
+};
+
+const getAllActivities = async (req, res) => {
+  try {
+    const count = await Activity.count();
+    if (count.length > 0) {
+      const activities = await Activity.findAll();
+      return res.status(200).json(activities);
+    } else {
+      return res.status(200).json({
+        success: true,
+        message: "No se han registrado actividades.",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const postActivity = async (req, res) => {
+  const { name, difficulty, duration, season, country } = req.body;
+  try {
+    const [newActivity, created] = await Activity.findOrCreate({
+      where: {
+        name,
+        defaults: {
+          name,
+          difficulty: Number(difficulty),
+          duration: Number(duration),
+          season: season.toString(),
+        },
+      },
+    });
+    if (created) {
+    }
+  } catch (error) {}
+};
+
+const putActivity = async (req, res) => {
+  const { id } = req.params;
+  const { name, difficulty, duration, season, country } = req.body;
+  try {
+    const activityToModify = await Activity.findByPk(id);
+    if (activityToModify) {
+      if (activityToModify.name !== name) {
+        activityToModify.name = name;
+      }
+      if (activityToModify.difficulty != difficulty) {
+        activityToModify.difficulty = Number(difficulty);
+      }
+      if (activityToModify.duration != duration) {
+        activityToModify.duration = Number(duration);
+      }
+      if (activityToModify.season != season) {
+        activityToModify.season = season.toString();
+      }
+
+      await activityToModify.save();
+
+      if (country.length > 0) {
+        await activityToModify.setCountries(country);
+      }
+      const modifiedActivity = await Activity.findByPk(id, {
+        include: {
+          model: Country,
+          through: { attributes: [] },
+          attributes: ["id"],
+        },
+      });
+
+      return res.status(200).json(modifiedActivity);
+    } else {
+      return res.status(404).json({ message: "Esta actividad no existe" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteActivity = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const activityToDelete = await Activity.findByPk(id);
+    if (activityToDelete) {
+      const associatedCountries = await activityToDelete.getCountries();
+      for (const country of associatedCountries) {
+        await activityToDelete.removeCountry(country);
+      }
+      await activityToDelete.destroy();
+      return res.status(200).json({
+        message: "La actividad ha sido borrada exitosamente.",
+      });
+    } else {
+      return res.status(404).json({ message: "Actividad no encontrada." });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getAllActivities,
+  postActivity,
+  putActivity,
+  deleteActivity,
+  dataComplete,
+};
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,16 +1,22 @@
 const { Router } = require("express");
-const { getAllCountries, getCountryById, getCountriesByName } = requiere(
-  "../controllers/countries"
-);
+const { getAllCountries, getCountryById, getCountriesByName, dataComplete } =
+  requiere("../controllers/countries");
+const {
+  getAllActivities,
+  postActivity,
+  putActivity,
+  deleteActivity,
+} = require("../controllers/activities");
+
 const router = Router();
 
 router.get("/countries", getAllCountries);
 router.get("/countries/:id", getCountryById);
 router.get("/countries/:name", getCountriesByName);
 
-router.get("/activities");
-router.post("/activities");
-router.put("/activities/:id");
-router.delete("/activities/:id");
+router.get("/activities", getAllActivities);
+router.post("/activities", dataComplete, postActivity);
+router.put("/activities/:id", dataComplete, putActivity);
+router.delete("/activities/:id", deleteActivity);
 
 module.exports = router;
