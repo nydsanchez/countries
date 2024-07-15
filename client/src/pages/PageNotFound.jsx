d941092228f9a8f1311899b6025ab5fc5a3190f7
Nydia Massiel Sánchez <nydsanchez@gmail.com>
2024-07-14 19:07:36 -0600

    chore: update path for NotFound component

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -3,23 +3,21 @@ import styles from "../styles/Pagenotfound.module.css";
 import { BsRocket } from "react-icons/bs";
 import { NavLink } from "react-router-dom";
 
-import PageNav from "../components/navpage/NavPage";
-import Band from "../components/SmartBar/Band";
-import Search from "../components/Search";
+import NavPage from "../components/navpage/NavPage";
+
+import SimpleOp from "../components/smartbar/SimpleOp";
 import Footer from "../components/footer/Footer";
+import image404 from "../assets/404.svg";
 
 function PageNotFound() {
   return (
     <div className={styles.NotFoundPage}>
       <header>
-        <PageNav />
-        <Band>Page not found!!</Band>
-        <div className={styles.bandSearch}>
-          <Search />
-        </div>
+        <NavPage />
+        <SimpleOp />
       </header>
       <main className={styles.main404}>
-        <img src="./404.svg" alt="code 404 not found" />
+        <img src={image404} alt="code 404 not found" />
         <p className={styles.pageNotFound}>
           Oops, it seems like you’re lost in space
         </p>
